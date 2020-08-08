684e89e3689e9a2bb7a7b66ee50083b9e8d460ab
Administrator <1184185491@qq.com>
2020-08-08 16:32:12 +0800

    git-learn

---
+++ src/GitTest.java
@@ -0,0 +1,11 @@
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("GITHello");
+    }
+}
+/**
+ * @program: jiqun
+ * @description:
+ * @author: WanZiWen
+ * @create: 2020-08-08 16:31
+ **/
